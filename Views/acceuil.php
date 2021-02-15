93c2e4e0a9488b618d054fa13b46ef2b40c030cc
rootkit7628 <arleme.dev7@gmail.com>
2021-02-15 13:52:47 +0300

    Create navbar

---
+++ Views/acceuil.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<title>ESTI | Login</title>
+  	<link rel="stylesheet" type="text/css" href="../Assets/css/acceuil.css">
+  </head>
+  <body>
+	<header id="header">
+		<nav class="nav">
+			<img src="../Assets/img/logo.png" id="logo" alt="Logo ESTI">
+			<a href="#">About us</a>
+			<a href="#">Message</a>
+			<a href="#">Proffesseur</a>
+			<a href="#">Etudiant</a>
+			<a href="#">Acceuil</a>
+		</nav>
+	</header>
+</body>
--- Views/login.php
+++ Views/login.php
@@ -11,24 +11,27 @@
     <main>
       <div id="container">
         <h2 class="title">Se connecter</h2>
-        <div><img src="../Assets/img/logo.png" id="icon" alt="User Icon" width="50px"/></div>
-        <!-- Login Form -->
-        <form>
-          <input type="email" id="login" name="login" placeholder="login">
-          <input type="password" id="password" name="login" placeholder="password">
-          <input type="submit" value="CONNEXION">
-        </form>
-        <!-- Select rôle -->
-        <div id="banner">
-          <select class="select">
-            <option value="0">Administrateur</option>
-            <option value="1" selected>Etudiant</option>
-            <option value="2">Professeur</option>
-          </select>
+        <div>
+          <img src="../Assets/img/logo.png" id="logo" alt="Logo ESTI"/>
         </div>
 
+        <!-- Login Form -->
+        <form method="POST" action="">
+          <input type="email" id="login" name="login" placeholder="Email ESTI" autocomplete="off">
+          <input type="password" id="password" name="login" placeholder="Mot de passe">
+          <input type="submit" value="connexion">
+        </form>
+
+        <!-- Select rôle -->
+          <div id="banner">
+            <select class="select">
+              <option value="0">Administrateur</option>
+              <option value="1" selected>Etudiant</option>
+              <option value="2">Professeur</option>
+            </select>
+          </div>
+        </div>
       </div>
-    </div>
     </main>
   </body>
 </html>
