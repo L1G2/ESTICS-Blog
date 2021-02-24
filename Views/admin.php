3262fe2c723b163f96f590d835a617b9d6baf25e
Offman075 <jean.offman.bienvenu@esti.mg>
2021-02-24 16:25:43 +0300

    create admin interface

---
+++ Views/admin.php
@@ -0,0 +1,117 @@
+ 
+<!DOCTYPE html>
+<html>
+  <head lang="fr">
+  	<title>ESTI | Login</title>
+	<meta charset="utf-8">
+  	<link rel="stylesheet" type="text/css" href="../Assets/css/admin.css">
+  </head>
+  <body>
+	<header id="header">
+		<nav class="nav">
+			<div id="container-user">
+				<div id="user-avatar">
+					<img src="../Assets/img/avatar.png" id="avatar" alt="-votre-avatar">
+				</div>
+				<div id="user-info">
+					<h4 id="name">Arlème Johson</h4>
+					<span id="email">arleme.dev7@gmail.com</span>
+					<h5 id="fontion">Administrateur</h5>
+				</div>
+			</div>
+			<div id="container-tool">
+				<button id="logout-btn">Deconnexion</button>
+			</div>
+		</nav>
+	</header>
+	<main>
+		<section id="panel" class="card">
+			<h4>Catégorie</h4>
+				<h5>Informatique</h5>
+					<ul>
+						<li>INFO_261</li>
+						<li>INFO_261</li>
+						<li>INFO_261</li>
+					</ul>
+				<h5>Communication</h5>
+					<ul>
+						<li>ENTR_261</li>
+						<li>ENTR_261</li>
+						<li>ENTR_261</li>
+					</ul>
+				<h5>Communication</h5>
+					<ul>
+						<li>ENTR_261</li>
+						<li>ENTR_261</li>
+						<li>ENTR_261</li>
+					</ul>
+		</section>
+		<section id="central" class="card">
+			<ul>
+				<li style="	float: left;"><h4><a href="#">Liste des étudiants</a></h4></li>
+				<li style="	float: right;"><h4><a href="#">Liste des profésseur</a></h4></li>
+			</ul>
+			<div>
+			<table id="tableau">
+				<tr>
+				  <th>Numero</th>
+				  <th>Nom et Prénom</th>
+				  <th>Email</th>
+				  <th>Modifier | Supprimer</th>
+				</tr>
+				<tr>
+					<td>01</td>
+					<td>Arlème Johnson</td>
+					<td>arleme.dev7@gmail.com</td>
+					<td><a>Supprimer</a> | <a>Modifier</a></td>	
+				</tr>
+				<tr>
+					<td>01</td>
+					<td>Arlème Johnson</td>
+					<td>arleme.dev7@gmail.com</td>
+					<td><a>Supprimer</a> | <a>Modifier</a></td>	
+				</tr>
+				<tr>
+					<td>01</td>
+					<td>Arlème Johnson</td>
+					<td>arleme.dev7@gmail.com</td>
+					<td><a>Supprimer</a> | <a>Modifier</a></td>	
+				</tr>
+				<tr>
+					<td>01</td>
+					<td>Arlème Johnson</td>
+					<td>arleme.dev7@gmail.com</td>
+					<td><a>Supprimer</a> | <a>Modifier</a></td>	
+				</tr>
+				<tr>
+					<td>01</td>
+					<td>Arlème Johnson</td>
+					<td>arleme.dev7@gmail.com</td>
+					<td><a>Supprimer</a> | <a>Modifier</a></td>	
+				</tr>
+				<tr>
+					<td>01</td>
+					<td>Arlème Johnson</td>
+					<td>arleme.dev7@gmail.com</td>
+					<td><a>Supprimer</a> | <a>Modifier</a></td>	
+				</tr>
+				<tr>
+					<td>01</td>
+					<td>Arlème Johnson</td>
+					<td>arleme.dev7@gmail.com</td>
+					<td><a>Supprimer</a> | <a>Modifier</a></td>	
+				</tr>
+				<tr>
+					<td>01</td>
+					<td>Arlème Johnson</td>
+					<td>arleme.dev7@gmail.com</td>
+					<td><a>Supprimer</a> | <a>Modifier</a></td>	
+				</tr>
+			  </table>
+			</div>
+		</section>
+	</main> 
+	<footer>
+
+	</footer>
+</body>
